1098e88c2f9b02b324a90ffe42d236195033d46f
秦真 <739694218@qq.com>
2019-12-20 18:05:20 +0800

    '群聊机器人发送消息'

--- src/robot.js
+++ src/robot.js
@@ -1,21 +1,62 @@
+const { qyweixin, zentao } = require('./config.json')
 const axios = require('axios')
+const fs = require('fs')
+const md5 = require('md5')
 
 const BASE_URL = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key='
-const ROBOT_KEY = 'd9323df8-930e-467b-9253-4db62f2dd1aa'
 
+// markdown消息类型
+const markdownMsg = {
+	msgtype: 'markdown',
+	markdown: {
+		content: `实时新增用户反馈<font color="warning">132例</font>，请相关同事注意。\n>类型:<font color="comment">用户反馈</font> \n>普通用户反馈:<font color="comment">117例</font> \n>VIP用户反馈:<font color="comment">15例</font>`
+	}
+}
+
+// 将本地图片转为 base64
+const bitmap = fs.readFileSync('report.png')
+const md5Str = md5(bitmap)
+const base64Str = Buffer.from(bitmap, 'binary').toString('base64')
+const imagePrefix = 'data:image/png;base64,'
+
+console.log('图片md5', md5Str)
+
+// 图片消息类型
+const imageMsg = {
+    msgtype: 'image',
+    image: {
+        base64: imagePrefix + base64Str,
+        md5: md5Str
+    }
+}
+
+// 图文消息类型
+const newsMsg = {
+    msgtype: 'news',
+    news: {
+       articles : [
+           {
+               title: '截至目前为止BUG分布情况',
+               description: '这些bug不解决掉，要留着过年嘛！',
+               url: zentao.myBugUrl,
+               picurl: 'http://res.mail.qq.com/node/ww/wwopenmng/images/independent/doc/test_pic_msg1.png'
+           },
+           {
+               title: '截至目前为止BUG分布情况',
+               description: '这些bug不解决掉，要留着过年嘛！',
+               url: zentao.myBugUrl,
+               picurl: 'http://res.mail.qq.com/node/ww/wwopenmng/images/independent/doc/test_pic_msg1.png'
+           },
+        ]
+    }
+}
 
 function sendMsg() {
 	axios({
-		url: BASE_URL + ROBOT_KEY,
+		url: BASE_URL + qyweixin.robotKeyTest,
 		method: 'POST',
-		data: {
-			msgtype: 'text',
-			text: {
-				content: 'Hello World'
-			}
-		}
+		data: imageMsg
 	})
-		// .then(res => res.response())
 		.then(res => {
 			console.log('发送结果', res.data)
 		})
