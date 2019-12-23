fb3d990a9d9a01ed89d840a6ecf690be2a1e5be3
秦真 <739694218@qq.com>
2019-12-23 21:15:48 +0800

    '企业q机器人'

--- src/robot.js
+++ src/robot.js
@@ -9,53 +9,18 @@ const BASE_URL = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key='
 const markdownMsg = {
 	msgtype: 'markdown',
 	markdown: {
-		content: `实时新增用户反馈<font color="warning">132例</font>，请相关同事注意。\n>类型:<font color="comment">用户反馈</font> \n>普通用户反馈:<font color="comment">117例</font> \n>VIP用户反馈:<font color="comment">15例</font>`
+		content: `#### 当前BUG共计 <font color="warning">132</font> 个 ，请及时[处理](http://113.108.117.211:19979/zentao/my-bug.html)。
+		\n>张三        **21**个 \n>空本给     **20**个 \n>张三张三 **20**个 \n>张三        **20**个 \n>张三        **20**个
+		`
 	}
 }
 
-// 将本地图片转为 base64
-const bitmap = fs.readFileSync('report.png')
-const md5Str = md5(bitmap)
-const base64Str = Buffer.from(bitmap, 'binary').toString('base64')
-const imagePrefix = 'data:image/png;base64,'
-
-console.log('图片md5', md5Str)
-
-// 图片消息类型
-const imageMsg = {
-    msgtype: 'image',
-    image: {
-        base64: imagePrefix + base64Str,
-        md5: md5Str
-    }
-}
-
-// 图文消息类型
-const newsMsg = {
-    msgtype: 'news',
-    news: {
-       articles : [
-           {
-               title: '截至目前为止BUG分布情况',
-               description: '这些bug不解决掉，要留着过年嘛！',
-               url: zentao.myBugUrl,
-               picurl: 'http://res.mail.qq.com/node/ww/wwopenmng/images/independent/doc/test_pic_msg1.png'
-           },
-           {
-               title: '截至目前为止BUG分布情况',
-               description: '这些bug不解决掉，要留着过年嘛！',
-               url: zentao.myBugUrl,
-               picurl: 'http://res.mail.qq.com/node/ww/wwopenmng/images/independent/doc/test_pic_msg1.png'
-           },
-        ]
-    }
-}
 
 function sendMsg() {
 	axios({
 		url: BASE_URL + qyweixin.robotKeyTest,
 		method: 'POST',
-		data: imageMsg
+		data: markdownMsg
 	})
 		.then(res => {
 			console.log('发送结果', res.data)
