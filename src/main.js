cbb0f338da5282032657972a8170e39a7e3c4679
秦真 <1097481534@qq.com>
2019-12-26 01:41:23 +0800

    完成统计bug并发送功能

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// const fs = require('fs')
+// const path = require('path')
+const md5 = require('md5')
+const { sendMsg } = require('./robot')
+const { unresolvedBugReport } = require('./unresolved-bug-report')
+
+async function main () {
+  const imageBuffer = await unresolvedBugReport()
+  // const imageBuffer = fs.readFileSync('../output/report.png')
+
+  const msg = {
+    msgtype: 'image',
+    image: {
+      base64: imageBuffer.toString('base64'), // 图片内容的base64编码
+      md5: md5(imageBuffer) // 图片内容（base64编码前）的md5值
+    }
+  }
+
+	const res = await sendMsg(msg)
+	console.log('发送结果', res.data)
+}
+
+main()
