1415e6bafa4e095b12d8b3fda26305a0b2ef0207
秦真 <1097481534@qq.com>
2019-12-27 01:13:52 +0800

    完成部门人员bug统计并发送

---
+++ src/send-dept.js
@@ -0,0 +1,24 @@
+
+const { sendMsg } = require('./robot')
+const { unresolvedBug } = require('./unresolved-bug')
+
+async function main () {
+  const bugs = await unresolvedBug()
+  console.log('当前bug情况', bugs)
+  const total = bugs.reduce((acc, item) => (acc += item.count), 0)
+  const content = bugs.map(bug => {
+    return `\n>${bug.name} \t\t **${bug.count}**个 `
+  }).join('')
+  const msg = {
+    msgtype: 'markdown',
+    markdown: {
+      content: `当前还有未解决的BUG共计 <font color="warning">${total}</font> 个 ，请以下同学及时[处理](http://113.108.117.211:19979/zentao/my-bug.html)。
+      ${content}
+      `
+    }
+  }
+	const res = await sendMsg(msg)
+	console.log('发送结果', res.data)
+}
+
+main()
---
+++ src/send-team.js
@@ -0,0 +1,20 @@
+
+const md5 = require('md5')
+const { sendMsg } = require('./robot')
+const { unresolvedBugReport } = require('./unresolved-bug-report')
+
+async function main () {
+  const imageBuffer = await unresolvedBugReport()
+
+  const msg = {
+    msgtype: 'image',
+    image: {
+      base64: imageBuffer.toString('base64'), // 图片内容的base64编码
+      md5: md5(imageBuffer) // 图片内容（base64编码前）的md5值
+    }
+  }
+	const res = await sendMsg(msg)
+	console.log('发送结果', res.data)
+}
+
+main()
